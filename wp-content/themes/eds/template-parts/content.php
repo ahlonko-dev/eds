f84e562dd7acb0f6613921004c342b58ee93f476
bertholdberthi <43133806+bertholdberthi@users.noreply.github.com>
2018-09-21 11:52:36 +0200

    ,njjnnk

--- wp-content/themes/eds/template-parts/content.php
+++ wp-content/themes/eds/template-parts/content.php
@@ -3,9 +3,9 @@
 
 <main class ="main-content">
         <div class ="fullwidth-block inner-content">
-                <div class ="container">
-                    <div class ="row">
-                            <div class ="col-md-6">
+                <div class ="container-fluid">
+                    <div class ="row navigator">
+                            <div class ="col-md-12">
                                 <div class ="content">
                                         <h2 class ="entry-title"><?php 
 the_title();?></h2>
