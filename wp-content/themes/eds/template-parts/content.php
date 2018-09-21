587daa38aa4b0d8d5f60b8d28d4b29be5541fe08
bertholdberthi <43133806+bertholdberthi@users.noreply.github.com>
2018-09-21 11:47:37 +0200

    jhjlkjkjlk

--- wp-content/themes/eds/template-parts/content.php
+++ wp-content/themes/eds/template-parts/content.php
@@ -1,1 +1,19 @@
-<?php the_content();
+
+
+
+<main class ="main-content">
+        <div class ="fullwidth-block inner-content">
+                <div class ="container">
+                    <div class ="row">
+                            <div class ="col-md-6">
+                                <div class ="content">
+                                        <h2 class ="entry-title"><?php 
+the_title();?></h2>
+                                        <?php the_content();?>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+        </div>
+</main>
+
